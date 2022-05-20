8a3ba99c5d7293ad0cac60dd0c439c7fa0c20a28
Virag Kormoczy <vkormoczy@gmail.com>
2022-05-20 23:52:14 +0300

    Add mobile menu

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// const cocktailURL = "www.thecocktaildb.com/api/json/v1/1/search.php?";
+const mobileMenuIcon = document.querySelector('.fa-bars');
+const closeMenuBtn = document.querySelector('.fa-angle-up');
+const mainSection = document.getElementById('main-section');
+
+closeMenuBtn.style.display = 'none';
+
+mobileMenuIcon.addEventListener('click', () => {
+  const mobileMenu = document.createElement('div');
+  mobileMenu.classList.add('mobile-div');
+  mobileMenu.innerHTML = `<div id="mobile-logo">
+  <i class="fas fa-cocktail"></i>
+  <span>Cocktails</span>
+</div>
+<div id="mobile-cocktails-list">
+  <span>Categories</span>
+  <fieldset>
+    <input
+      type="search"
+      id="mobile-search"
+      name="search"
+      placeholder="Search cocktail..."
+    />
+    <i class="fas fa-search"></i>
+  </fieldset>
+</div>`;
+
+  mainSection.appendChild(mobileMenu);
+
+  mobileMenuIcon.style.display = 'none';
+  closeMenuBtn.style.display = 'block';
+  closeMenuBtn.classList.add('menu');
+});
+
+closeMenuBtn.addEventListener('click', () => {
+  const mobileMenuActive = document.querySelector('.mobile-div');
+  mobileMenuActive.remove();
+  closeMenuBtn.style.display = 'none';
+  mobileMenuIcon.style.display = 'block';
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 610) {
+    document.location.reload();
+  }
+});
