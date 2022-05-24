68db58f7d7730934196f9cf0d310f7c61065dfd8
Virag Kormoczy <vkormoczy@gmail.com>
2022-05-24 18:45:19 +0300

    Fix linter issues

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
 import {
-  mainSection,
   categoriesSection,
+  cocktailsSection,
+  cocktailDetails,
   leftArrow,
-  getDrinks,
-} from "./modules/getDrinks.js";
+  mainSection,
+} from './modules/getDrinks.js';
 
-import getCocktailName from "./modules/searchCocktail.js";
+import getCocktailName from './modules/searchCocktail.js';
 
-const mobileMenuIcon = document.querySelector(".fa-bars");
-const closeMenuBtn = document.querySelector(".fa-angle-up");
-const categoriesSpan = document.getElementById("categories-span");
-const gridContainer = document.getElementById("grid-container");
-const search = document.getElementById("search");
+const mobileMenuIcon = document.querySelector('.fa-bars');
+const closeMenuBtn = document.querySelector('.fa-angle-up');
+const categoriesSpan = document.getElementById('categories-span');
 
-closeMenuBtn.style.display = "none";
-categoriesSection.style.display = "none";
+closeMenuBtn.style.display = 'none';
+categoriesSection.style.display = 'none';
+cocktailsSection.style.display = 'none';
+cocktailDetails.style.display = 'none';
+
+const search = document.getElementById('search');
 
 // Search event listener
-search.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    if (search.value === "") {
+search.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    if (search.value === '') {
       return;
     }
     getCocktailName(search.value.trim());
-    search.value = "";
+    search.value = '';
   }
 });
 
-// Categories divs event listener
-gridContainer.addEventListener("click", (e) => {
-  getDrinks(e);
-});
-
 // Mobile menu (hamburger menu) event listener
-mobileMenuIcon.addEventListener("click", () => {
-  const mobileMenu = document.createElement("div");
+mobileMenuIcon.addEventListener('click', () => {
+  const mobileMenu = document.createElement('div');
   mobileMenu.innerHTML = `<div id="mobile-logo">
   <i class="fas fa-cocktail"></i>
   <span>Cocktails</span>
@@ -52,42 +50,42 @@ mobileMenuIcon.addEventListener("click", () => {
   </fieldset>
 </div>`;
 
-  mobileMenu.classList.add("mobile-div");
+  mobileMenu.classList.add('mobile-div');
   mainSection.appendChild(mobileMenu);
-  mobileMenuIcon.style.display = "none";
-  closeMenuBtn.style.display = "block";
-  closeMenuBtn.classList.add("menu");
+  mobileMenuIcon.style.display = 'none';
+  closeMenuBtn.style.display = 'block';
+  closeMenuBtn.classList.add('menu');
 
-  const mobileCategories = document.getElementById("mobile-categories");
-  mobileCategories.addEventListener("click", () => {
-    mainSection.style.display = "none";
-    categoriesSection.style.display = "flex";
+  const mobileCategories = document.getElementById('mobile-categories');
+  mobileCategories.addEventListener('click', () => {
+    mainSection.style.display = 'none';
+    categoriesSection.style.display = 'flex';
   });
 });
 
 // Arrow up icon event listener on mobile menu
-closeMenuBtn.addEventListener("click", () => {
-  const mobileMenuActive = document.querySelector(".mobile-div");
+closeMenuBtn.addEventListener('click', () => {
+  const mobileMenuActive = document.querySelector('.mobile-div');
   mobileMenuActive.remove();
-  closeMenuBtn.style.display = "none";
-  mobileMenuIcon.style.display = "block";
+  closeMenuBtn.style.display = 'none';
+  mobileMenuIcon.style.display = 'block';
 });
 
 // While the mobile menu is open and when the user resizes the screen
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
   if (window.innerWidth < 610) {
     document.location.reload();
   }
 });
 
 // Categories link event listener
-categoriesSpan.addEventListener("click", () => {
-  mainSection.style.display = "none";
-  categoriesSection.style.display = "grid";
+categoriesSpan.addEventListener('click', () => {
+  mainSection.style.display = 'none';
+  categoriesSection.style.display = 'grid';
 });
 
 // Left arrow event listener
-leftArrow.addEventListener("click", () => {
-  categoriesSection.style.display = "none";
-  mainSection.style.display = "block";
+leftArrow.addEventListener('click', () => {
+  categoriesSection.style.display = 'none';
+  mainSection.style.display = 'block';
 });
--- modules/searchCocktail.js
+++ modules/searchCocktail.js
@@ -1,30 +1,31 @@
-import { cocktailsSection, mainSection } from "./getDrinks.js";
-
-import { getDetails, cocktailDetails } from "./getDetails.js";
+import {
+  cocktailsSection,
+  mainSection,
+  cocktailDetails,
+  getDetails,
+} from './getDrinks.js';
 
 const getCocktailName = (value) => {
-  cocktailsSection.style.display = "block";
-  mainSection.style.display = "none";
+  cocktailsSection.style.display = 'block';
+  mainSection.style.display = 'none';
 
-  let query1 = `s=${value}`;
+  const query1 = `s=${value}`;
 
-  cocktailsSection.innerHTML = `<i class="fas fa-arrow-left" id="cocktails-arrow"></i>`;
+  cocktailsSection.innerHTML = '<i class="fas fa-arrow-left" id="cocktails-arrow"></i>';
 
-  const cocktailsArrow = document.getElementById("cocktails-arrow");
+  const cocktailsArrow = document.getElementById('cocktails-arrow');
 
-  cocktailsArrow.addEventListener("click", () => {
-    cocktailsSection.innerHTML = "";
-    cocktailsSection.style.display = "none";
-    mainSection.style.display = "block";
+  cocktailsArrow.addEventListener('click', () => {
+    cocktailsSection.innerHTML = '';
+    cocktailsSection.style.display = 'none';
+    mainSection.style.display = 'block';
   });
 
   fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?${query1}`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-
       data.drinks.forEach((item) => {
-        const cocktailsDiv = document.createElement("div");
+        const cocktailsDiv = document.createElement('div');
         cocktailsDiv.innerHTML = `
       <div class='cocktails'>
       <img src=${item.strDrinkThumb} alt="drink" id=${item.idDrink}>
@@ -33,19 +34,15 @@ const getCocktailName = (value) => {
       `;
         cocktailsSection.appendChild(cocktailsDiv);
       });
-      const cocktailDrinks = [...document.querySelectorAll(".cocktails > img")];
+      const cocktailDrinks = [...document.querySelectorAll('.cocktails > img')];
 
-      cocktailDrinks.forEach((drink) =>
-        drink.addEventListener("click", (e) => {
-          cocktailsSection.style.display = "none";
-          cocktailDetails.style.display = "block";
-          getDetails(e);
-        })
-      );
+      cocktailDrinks.forEach((drink) => drink.addEventListener('click', (e) => {
+        cocktailsSection.style.display = 'none';
+        cocktailDetails.style.display = 'block';
+        getDetails(e);
+      }));
     })
-    .catch((err) => {
-      console.log(`error ${err}`);
-    });
+    .catch();
 };
 
 export default getCocktailName;
